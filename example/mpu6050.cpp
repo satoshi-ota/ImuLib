8913da34b192754cf0dfea58498cc1a77fff9dda
satoshi ota <satoshi.ota928@gmail.com>
2019-10-18 23:53:55 +0900

    Initial commit

---
+++ example/mpu6050.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <time.h>
+#include <MPU6050.h>
+
+int main()
+{
+    MPU6050 *mpu6050 = new MPU6050();
+    int err = mpu6050->openMPU6050();
+
+    if(err < 0)
+    {
+        printf("Error: %d", mpu6050->error);
+    }
+    else
+    {
+        printf("MPU6050 Device Address: 0x%02X\n", mpu6050->kI2CAddress);
+        //for debugging
+        int devAddress = mpu6050->readByte(MPU6050_WHO_AM_I);
+        printf("I am 0x%02X\n", devAddress);
+
+        sleep(1);
+    }
+    mpu6050->closeMPU6050();
+}
---
+++ src/MPU6050.cpp
@@ -0,0 +1,68 @@
+#include <math.h>
+
+#include <MPU6050.h>
+
+MPU6050::MPU6050(int address)
+{
+    kI2CBus = 0;
+    kI2CAddress = address;
+    error = 0;
+}
+
+MPU6050::~MPU6050()
+{
+    closeMPU6050();
+}
+
+bool MPU6050::openMPU6050()
+{
+    char fileNameBuffer[32];
+    sprintf(fileNameBuffer, "/dev/i2c-%d", kI2CBus);
+    kI2CFileDescriptor = open(fileNameBuffer, O_RDWR);
+
+    if(kI2CFileDescriptor < 0)
+    {
+        error = errno;
+        return false;
+    }
+
+    if(ioctl(kI2CFileDescriptor, I2C_SLAVE, kI2CAddress) < 0)
+    {
+        error = errno;
+        return false;
+    }
+    return true;
+}
+
+void MPU6050::closeMPU6050()
+{
+    if(kI2CFileDescriptor > 0)
+    {
+        close(kI2CFileDescriptor);
+        kI2CFileDescriptor = -1;
+    }
+}
+
+int MPU6050::readByte(int readRegister)
+{
+    int toReturn = i2c_smbus_read_byte_data(kI2CFileDescriptor, readRegister);
+    if(toReturn < 0)
+    {
+        printf("MPU6050 Read Byte Error: %d", errno);
+        error = errno;
+        toReturn = -1;
+    }
+    return toReturn;
+}
+
+int MPU6050::writeByte(int writeRegister, int writevalue)
+{
+    int toReturn = i2c_smbus_write_byte_data(kI2CFileDescriptor, writeRegister, writeValue);
+    if(toReturn < 0)
+    {
+        printf("MPU6050 Write Byte Error: %d", errno);
+        error = errno;
+        toReturn = -1;
+    }
+    return toReturn;
+}
